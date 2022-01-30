60302fe14babdbb111ccf76819be3cf4ba433531
Benito Ramos <daniel.ramos06sql@gmail.com>
2022-01-30 09:08:56 -0600

    Add file installations

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # PySpark
+
+## Installation
+
+
+### Install Python 
+
+### Download Spark
+
+We must download Spark file from https://spark.apache.org/downloads.html
+After we downloaded the file, we must to do next steps:
+
+* 
+
+
---
+++ test.py
@@ -0,0 +1,18 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster('local').setAppName('Test')
+sc = SparkContext(conf = conf)
+
+import random
+num_samples = 100000000
+
+def inside(p):     
+  x, y = random.random(), random.random()
+  return x*x + y*y < 1
+
+count = sc.parallelize(range(0, num_samples)).filter(inside).count()
+
+pi = 4 * count / num_samples
+print(pi)
+
+sc.stop()
