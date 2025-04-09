c9fcd2ca14c28cc4973a2726a86c5b760269f4b0
BenRamo06 <90486154+BenRamo06@users.noreply.github.com>
2025-04-09 13:32:18 -0600

    Update 04.-Read_something.py

--- pipeline/Dataframe/04.-Read_something.py
+++ pipeline/Dataframe/04.-Read_something.py
@@ -3,19 +3,34 @@ from pyspark.sql.types import StructType, StructField
 from pyspark.sql.types import StringType, IntegerType
 
 
-spark = SparkSession.builder.master("local").appName('Read Something').getOrCreate()
+# Define our Spark session
+spark = SparkSession.builder.master("local") \
+                    .appName('Read Something') \
+                    .getOrCreate()
 
+# Define our Dataframe structure
 schema_data = StructType([
                           StructField("Character", StringType(), False),
                           StructField("Movie", IntegerType(), False)
                         ])
 
-data = spark.read.csv('/home/benito.ramos/Desktop/GIT/PySpark/inputs/data_movies',inferSchema=True, header=False )
+# Read our file (Three ways to do)
+data = spark.read.csv("inputs/data_movies", inferSchema=True, header=False )
 
-data = spark.read.format("csv").options(inferSchema=True, header=False).schema(schema_data).load('inputs/data_movies')
+data = spark.read \
+            .format("csv") \
+            .options(inferSchema=True, header=False).schema(schema_data) \
+            .load("inputs/data_movies")
+
+data = spark.read \
+            .format("csv") \
+            .option("inferSchema", "true") \
+            .option("header", "true") \
+            .option("schema", schema_data) \
+            .load("inputs/data_movies")
+
 
 data.show()
-
 data.printSchema()
 
 spark.stop()
